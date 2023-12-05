d4a94d625b8db04ab0b1f0b8286297a63157ffd5
GregEnForce <gregoire.dupont@viacesi.fr>
2023-12-05 16:19:35 +0100

    oui ?

--- PageClient.h
+++ PageClient.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 namespace ProjetPOO {
 
@@ -8,6 +9,7 @@ namespace ProjetPOO {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace std;
 
 	/// <summary>
 	/// Description résumée de PageClient
@@ -44,6 +46,7 @@ namespace ProjetPOO {
 	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::RichTextBox^ richTextBox1;
 	private: System::Windows::Forms::ContextMenuStrip^ contextMenuStrip1;
+	private: System::Windows::Forms::ListView^ listView1;
 	private: System::ComponentModel::IContainer^ components;
 	public:
 
@@ -81,8 +84,15 @@ namespace ProjetPOO {
 		{
 			this->components = (gcnew System::ComponentModel::Container());
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(PageClient::typeid));
+			System::Windows::Forms::ListViewGroup^ listViewGroup1 = (gcnew System::Windows::Forms::ListViewGroup(L"ListViewGroup", System::Windows::Forms::HorizontalAlignment::Left));
+			System::Windows::Forms::ListViewGroup^ listViewGroup2 = (gcnew System::Windows::Forms::ListViewGroup(L"ListViewGroup", System::Windows::Forms::HorizontalAlignment::Left));
+			System::Windows::Forms::ListViewGroup^ listViewGroup3 = (gcnew System::Windows::Forms::ListViewGroup(L"ListViewGroup", System::Windows::Forms::HorizontalAlignment::Left));
+			System::Windows::Forms::ListViewItem^ listViewItem1 = (gcnew System::Windows::Forms::ListViewItem(L"Ta mère"));
+			System::Windows::Forms::ListViewItem^ listViewItem2 = (gcnew System::Windows::Forms::ListViewItem(L"Amour"));
+			System::Windows::Forms::ListViewItem^ listViewItem3 = (gcnew System::Windows::Forms::ListViewItem(L"Allô \?"));
 			this->OngletsClient = (gcnew System::Windows::Forms::TabControl());
 			this->Commande = (gcnew System::Windows::Forms::TabPage());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->Articles = (gcnew System::Windows::Forms::ListBox());
 			this->Recherche = (gcnew System::Windows::Forms::TextBox());
@@ -90,7 +100,7 @@ namespace ProjetPOO {
 			this->Panier = (gcnew System::Windows::Forms::TabPage());
 			this->imageList1 = (gcnew System::Windows::Forms::ImageList(this->components));
 			this->contextMenuStrip1 = (gcnew System::Windows::Forms::ContextMenuStrip(this->components));
-			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
 			this->OngletsClient->SuspendLayout();
 			this->Commande->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
@@ -109,6 +119,7 @@ namespace ProjetPOO {
 			// 
 			// Commande
 			// 
+			this->Commande->Controls->Add(this->listView1);
 			this->Commande->Controls->Add(this->richTextBox1);
 			this->Commande->Controls->Add(this->pictureBox1);
 			this->Commande->Controls->Add(this->Articles);
@@ -122,6 +133,15 @@ namespace ProjetPOO {
 			this->Commande->Text = L"Commande";
 			this->Commande->UseVisualStyleBackColor = true;
 			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(640, 43);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->ReadOnly = true;
+			this->richTextBox1->Size = System::Drawing::Size(312, 132);
+			this->richTextBox1->TabIndex = 3;
+			this->richTextBox1->Text = L"Nom article\n\nPrix TTC\n\nDescription\n\nQuantité\t\t\t\tCouleur";
+			// 
 			// pictureBox1
 			// 
 			this->pictureBox1->ErrorImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.ErrorImage")));
@@ -134,7 +154,6 @@ namespace ProjetPOO {
 			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox1->TabIndex = 2;
 			this->pictureBox1->TabStop = false;
-			//this->pictureBox1->Image =: imageList1->Images[0];
 			// 
 			// Articles
 			// 
@@ -150,6 +169,7 @@ namespace ProjetPOO {
 			this->Articles->Name = L"Articles";
 			this->Articles->ScrollAlwaysVisible = true;
 			this->Articles->Size = System::Drawing::Size(261, 420);
+			this->Articles->Sorted = true;
 			this->Articles->TabIndex = 1;
 			this->Articles->SelectedIndexChanged += gcnew System::EventHandler(this, &PageClient::listBox1_SelectedIndexChanged);
 			// 
@@ -160,6 +180,7 @@ namespace ProjetPOO {
 			this->Recherche->Size = System::Drawing::Size(261, 22);
 			this->Recherche->TabIndex = 0;
 			this->Recherche->Text = L"Rechercher";
+			this->Recherche->TextChanged += gcnew System::EventHandler(this, &PageClient::Recherche_TextChanged);
 			// 
 			// Compte
 			// 
@@ -194,14 +215,43 @@ namespace ProjetPOO {
 			this->contextMenuStrip1->Name = L"contextMenuStrip1";
 			this->contextMenuStrip1->Size = System::Drawing::Size(61, 4);
 			// 
-			// richTextBox1
+			// listView1
 			// 
-			this->richTextBox1->Location = System::Drawing::Point(640, 43);
-			this->richTextBox1->Name = L"richTextBox1";
-			this->richTextBox1->ReadOnly = true;
-			this->richTextBox1->Size = System::Drawing::Size(312, 132);
-			this->richTextBox1->TabIndex = 3;
-			this->richTextBox1->Text = L"Nom article\n\nPrix TTC\n\nDescription\n\nQuantité\t\t\t\tCouleur";
+			listViewGroup1->Header = L"ListViewGroup";
+			listViewGroup1->Name = L"Mathieu";
+			listViewGroup2->Header = L"ListViewGroup";
+			listViewGroup2->Name = L"Martin";
+			listViewGroup3->Header = L"ListViewGroup";
+			listViewGroup3->Name = L"Greg";
+			this->listView1->Groups->AddRange(gcnew cli::array< System::Windows::Forms::ListViewGroup^  >(3) {
+				listViewGroup1, listViewGroup2,
+					listViewGroup3
+			});
+			this->listView1->HideSelection = false;
+			listViewItem3->Group = listViewGroup1;
+			this->listView1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ListViewItem^  >(3) {
+				listViewItem1, listViewItem2,
+					listViewItem3
+			});
+			this->listView1->Location = System::Drawing::Point(300, 79);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(298, 198);
+			this->listView1->TabIndex = 4;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			this->listView1->View = System::Windows::Forms::View::List;
+			this->listView1->SelectedIndexChanged += gcnew System::EventHandler(this, &PageClient::listView1_SelectedIndexChanged_1);
+
+			void searchBox_TextChanged(object sender, EventArgs e)
+			{
+				// Call FindItemWithText with the contents of the textbox.
+				ListViewItem foundItem =
+					listView1->FindItemWithText(Recherche->Text, false, 0, true);
+				if (foundItem != null)
+				{
+					listView1->TopItem = foundItem;
+				}
+			}
+
 			// 
 			// PageClient
 			// 
@@ -233,6 +283,10 @@ private: System::Void treeView1_AfterSelect(System::Object^ sender, System::Wind
 private: System::Void listView1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void Recherche_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void listView1_SelectedIndexChanged_1(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
